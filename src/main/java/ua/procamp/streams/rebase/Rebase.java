7c15f20cc618399ce326c3b57243fe9625b6598d
m.skafenko <misha41192@gmail.com>
2021-01-28 15:49:50 +0200

    commit rebase #3

--- src/main/java/ua/procamp/streams/rebase/Rebase.java
+++ src/main/java/ua/procamp/streams/rebase/Rebase.java
@@ -2,4 +2,12 @@ package ua.procamp.streams.rebase;
 
 public class Rebase {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
